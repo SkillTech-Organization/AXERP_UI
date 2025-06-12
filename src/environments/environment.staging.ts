2f81806eac71e2e751f54dddc2b0f3f9aa2561c8
kjeremi <kdrisaac42@gmail.com>
2025-06-12 19:23:53 +0200

    environment.staging.ts and related settings added, cicd environment handling updated to use staging

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    apiUrl: "https://axerpazurefunctiontest.azurewebsites.net/",
+    test: "development",
+    msalConfig: {
+        auth: {
+            clientId: '',
+            authority: '',
+        },
+    },
+    apiConfig: {
+        redirect: "https://test.axerp.axegaz.com/index.html",
+        scopes: [''],
+        uri: "https://axerpazurefunctiontest.azurewebsites.net/"
+    },
+};
--- update-environment.js
+++ update-environment.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-let content = fs.readFileSync('src/environments/environment.ts', 'utf8');
+let content = fs.readFileSync('src/environments/environment.staging.ts', 'utf8');
 
 // Environment variables from process.env
 const {
@@ -24,5 +24,5 @@ content = content
   .replace(/uri:\s*["'`].*?["'`]/, `uri: "${URI}"`)
   .replace(/scopes:\s*\[[^\]]*\]/, scopesString);
 
-fs.writeFileSync('src/environments/environment.ts', content);
-console.log('environment.ts updated successfully');
+fs.writeFileSync('src/environments/environment.staging.ts', content);
+console.log('environment.staging.ts updated successfully');
